1557cb6dc66cc6c4a6cabe05582269ba25e14e24
Nash232305 <2021051418@estudiantec.cr>
2023-08-19 02:42:07 -0600

    Codigo tarea 2

--- tareas/Weather-app/src/main/java/tec/bd/weather/service/WeatherService.java
+++ tareas/Weather-app/src/main/java/tec/bd/weather/service/WeatherService.java
@@ -1,7 +1,17 @@
 package tec.bd.weather.service;
 
+import tec.bd.weather.entity.Forecast;
+
 public interface WeatherService {
 
     float getCityTemperature(String city);
     float getZipCodeTemperature(String zipCode);
+    
+    void newForecast (Forecast weather);
+    
+    Forecast updateForecast(Forecast weather);
+    
+    void removeForecast(int forecastId);
+
+    
 }
