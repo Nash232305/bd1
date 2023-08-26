2b461c7fbadcd96eb21c9d836a9f9b58b6fd375f
Nash <116378833+Nash232305@users.noreply.github.com>
2023-08-26 01:14:36 -0600

    nuevas pruebas unitarias

--- tareas/weather-app/src/main/java/tec/bd/weather/repository/InMemoryForecastRepository.java
+++ tareas/weather-app/src/main/java/tec/bd/weather/repository/InMemoryForecastRepository.java
@@ -1,71 +1,62 @@
 package tec.bd.weather.repository;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import tec.bd.weather.entity.Forecast;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 
-public class InMemoryForecastRepository implements Repository<Forecast, Integer>{
-    
-    private Set<Forecast> inMemoryWeatherData;
-    
-    
+public class InMemoryForecastRepository implements  Repository<Forecast, Integer> {
+
+    private Set<Forecast> inMemoryForecastData;
+
     public InMemoryForecastRepository(){
-        //Inicializando la base de datos
-        this.inMemoryWeatherData = new HashSet<>();
-        
-        this.inMemoryWeatherData.add(new Forecast(1,"Costa Rica","Alajuela","10101",23.0f));
-        this.inMemoryWeatherData.add(new Forecast(2,"Costa Rica","Cartago","20201",24.0f));
-        this.inMemoryWeatherData.add(new Forecast(3,"Costa Rica","San Jose","30301",25.0f));
-        this.inMemoryWeatherData.add(new Forecast(4,"Costa Rica","Limon","40401",26.0f));
+        //inicializando la bases de datos
+        this.inMemoryForecastData = new HashSet<>();
+        this.inMemoryForecastData.add(new Forecast(1, "Costa Rica", "Alajuela", "10101", new Date(), 23.0f));
+        this.inMemoryForecastData.add(new Forecast(2, "Costa Rica", "Cartago", "20201", new Date(), 24.0f));
+        this.inMemoryForecastData.add(new Forecast(3, "Costa Rica", "San Jose", "30301", new Date(), 25.0f));
+        this.inMemoryForecastData.add(new Forecast(4, "Costa Rica", "Limon", "40401", new Date(), 25.0f));
     }
-    
 
     @Override
     public Optional <Forecast> findById(Integer id) {
-        return this.inMemoryWeatherData
-            .stream()
-            .filter(e -> Objects.equals(e.getId(), id))
-            .findFirst();
+        return this.inMemoryForecastData
+                .stream()
+                .filter(e -> Objects.equals(e.getId(),id))
+                .findFirst();
     }
-     
 
     @Override
     public List<Forecast> findAll() {
-        return new ArrayList<>(this.inMemoryWeatherData);
+        return new ArrayList<>(this.inMemoryForecastData);
     }
 
     @Override
-    public void save(Forecast weather) {
-        this.inMemoryWeatherData.add(weather);
+    public void save(Forecast forecast) {
+        this.inMemoryForecastData.add(forecast);
     }
 
     @Override
     public void delete(Integer id) {
         var weatherToDelete = this.findById(id);
-        this.inMemoryWeatherData.remove(weatherToDelete.get());
-    
+        this.inMemoryForecastData.remove(weatherToDelete.get());
     }
 
     @Override
     public Forecast update(Forecast source) {
+        System.out.println("1");
         var current = this.findById(source.getId()).get();
-       
         current.setCityName(source.getCityName());
         current.setZipCode(source.getZipCode());
         current.setTemperature(source.getTemperature());
-        
+
         this.delete(current.getId());
         this.save(current);
-        
+
         return current;
     }
+}
+
 
    
 
-}
