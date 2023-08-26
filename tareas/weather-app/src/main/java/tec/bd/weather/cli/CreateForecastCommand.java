23bf7497f5bb656bbe0a6b317490c2d5c80d89a6
Nash <116378833+Nash232305@users.noreply.github.com>
2023-08-26 01:05:48 -0600

    nuevas pruebas unitarias

--- tareas/weather-app/src/main/java/tec/bd/weather/cli/CreateForecastCommand.java
+++ tareas/weather-app/src/main/java/tec/bd/weather/cli/CreateForecastCommand.java
@@ -3,42 +3,44 @@ package tec.bd.weather.cli;
 import picocli.CommandLine;
 import tec.bd.weather.ApplicationContext;
 import tec.bd.weather.entity.Forecast;
+import java.util.Date;
 
 /**
  *
  * @author henge
  */
+@CommandLine.Command(name = "create-forecast", aliases = {"cf"}, description = "Create new forecast for a city")
+public class CreateForecastCommand implements Runnable {
 
-@CommandLine.Command(name ="create-forecast", aliases ={"cf"},description ="Create new forecast for a city")
-public class CreateForecastCommand  implements Runnable{
-    
-    @CommandLine.Parameters(paramLabel ="forecast id", description ="The new forecast id")
+    @CommandLine.Parameters(paramLabel = "<forecast id>", description = "The new forecast id")
     private int newForecastId;
 
-    @CommandLine.Parameters(paramLabel ="country name",description ="The country name")
+    @CommandLine.Parameters(paramLabel = "<country name>", description = "The country name")
     private String countryName;
-    
-    @CommandLine.Parameters(paramLabel ="city name",description ="The city name")
+
+    @CommandLine.Parameters(paramLabel = "<city name>", description = "The city name")
     private String cityName;
-    
-    @CommandLine.Parameters(paramLabel ="zip code",description ="The zip code")
+    @CommandLine.Parameters(paramLabel = "<zip code>", description = "The Zip code")
     private String zipCode;
 
-    
-    @CommandLine.Parameters(paramLabel ="<temperature>",description ="Temperature value")
+    @CommandLine.Parameters(paramLabel = "<forecast date>", description = "The forecast date")
+    private Date date;
+
+    @CommandLine.Parameters(paramLabel = "<temperature>", description = "Temperature value")
     private float temperature;
-    
+
+
+
     @Override
     public void run() {
-        try{
+        try {
             var appContext = new ApplicationContext();
             var weatherService = appContext.getWeatherService();
-            var newForecast = new Forecast (newForecastId, countryName,cityName,zipCode,temperature);
+            var newForecast = new Forecast(newForecastId, countryName, cityName, zipCode,date, temperature);
             weatherService.newForecast(newForecast);
             System.out.println(newForecast);
-        }catch (Exception e){
-            System.err.println("Can't create forecast. "+ e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Can't create forecast. " +  e.getMessage());
         }
     }
-    
 }
