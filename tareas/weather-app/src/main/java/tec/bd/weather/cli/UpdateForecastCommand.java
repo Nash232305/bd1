14632ef16a3ce9bcb2558c0c146ea7509aa23ec5
Nash <116378833+Nash232305@users.noreply.github.com>
2023-08-26 01:07:13 -0600

    nuevas pruebas unitarias

--- tareas/weather-app/src/main/java/tec/bd/weather/cli/UpdateForecastCommand.java
+++ tareas/weather-app/src/main/java/tec/bd/weather/cli/UpdateForecastCommand.java
@@ -1,47 +1,51 @@
-
 package tec.bd.weather.cli;
 
 import picocli.CommandLine;
 import tec.bd.weather.ApplicationContext;
 import tec.bd.weather.entity.Forecast;
+import java.util.Date;
+
+import java.util.Date;
 
 /**
  *
  * @author henge
  */
 
-@CommandLine.Command(name = "update-forecast", aliases ={"uf"}, description= "Update existing forecast data")
+
+@CommandLine.Command(name = "update-forecast", aliases = { "uf" }, description = "Update existing forecast data")
 public class UpdateForecastCommand implements Runnable {
 
-    
     @CommandLine.Parameters(paramLabel = "<forecast id>", description = "The new forecast id")
     private int newForecastId;
-    
+
     @CommandLine.Parameters(paramLabel = "<country name>", description = "The country name")
     private String countryName;
-    
+
     @CommandLine.Parameters(paramLabel = "<city name>", description = "The city name")
     private String cityName;
-    
-    @CommandLine.Parameters(paramLabel = "<zip code", description = "The Zip code")
+    @CommandLine.Parameters(paramLabel = "<zip code>", description = "The Zip code")
     private String zipCode;
-    
-    @CommandLine.Parameters(paramLabel = "temperature", description = "Temperature value")
+
+    @CommandLine.Parameters(paramLabel = "<forecast date>", description = "The forecast date")
+    private Date date;
+
+    @CommandLine.Parameters(paramLabel = "<temperature>", description = "Temperature value")
     private float temperature;
-
-
 
     @Override
     public void run() {
-        try{
+        try {
             var appContext = new ApplicationContext();
             var weatherService = appContext.getWeatherService();
-            var newForecast = new Forecast (newForecastId,countryName,cityName,zipCode,temperature);
-            var updatedForecast = weatherService.updateForecast(newForecast);
+            var newForecast = new Forecast(newForecastId, countryName, cityName, zipCode,date,temperature);
+            var  updatedForecast = weatherService.updateForecast(newForecast);
             System.out.println(updatedForecast);
-        }catch (Exception e){
-            System.out.println("Can't updated forecast "+ e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Can't upated forecast. " +  e.getMessage());
         }
     }
+
+}
     
 }
